03a18a516795cc8c99c7061f2a7ee1bf10aa2949
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2022-06-14 21:53:49 -0400

    fix(typescript): needed to get typescript error to go away

--- babel.config.js
+++ babel.config.js
@@ -43,6 +43,7 @@ module.exports = {
     '@babel/typescript',
   ],
   plugins: [
+    ['@babel/plugin-transform-typescript', { allowDeclareFields: true }],
     /**
      * NOTE
      * Experimental decorators are used in `@redwoodjs/structure`.
