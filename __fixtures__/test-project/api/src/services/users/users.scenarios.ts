8eea0676ed038822c082ac54482fcba3579855c1
Peter Colapietro <petercolapietro+spacehippie@gmail.com>
2022-08-27 14:13:38 -0400

    chore(fixtures): update
    ran `yarn build:test-project --rebuild-fixture`
    
    manually updated `__fixtures__/test-project/package.json`

--- __fixtures__/test-project/api/src/services/users/users.scenarios.ts
+++ __fixtures__/test-project/api/src/services/users/users.scenarios.ts
@@ -6,7 +6,7 @@ export const standard = defineScenario<Prisma.UserCreateArgs>({
   user: {
     one: {
       data: {
-        email: 'String8646974',
+        email: 'String4982989',
         hashedPassword: 'String',
         fullName: 'String',
         salt: 'String',
@@ -14,7 +14,7 @@ export const standard = defineScenario<Prisma.UserCreateArgs>({
     },
     two: {
       data: {
-        email: 'String1817344',
+        email: 'String1478824',
         hashedPassword: 'String',
         fullName: 'String',
         salt: 'String',
